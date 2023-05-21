e7346d379681f4648cb205c42fc9aceef91f4a7b
Umair Syed <syedumairandrabi66@gmail.com>
2023-05-21 10:41:37 +0530

    some updates

--- trace-viz-backend/mainapp/map.py
+++ trace-viz-backend/mainapp/map.py
@@ -1,41 +1,6 @@
 import folium
 from folium.utilities import validate_location
 
-# def plot(locations):
-#     """Plots locations on a map with different colors based on the city name."""
-#     # Initialize the map
-#     m = folium.Map(location=[0, 0], zoom_start=2)
-    
-#     # Create a color map based on the city name
-#     city_colors = {}
-#     for loc in locations:
-#         city = loc[3]
-#         if city not in city_colors:
-#             color = '#{:06x}'.format(hash(city) % 0xffffff) 
-#             city_colors[city] = color
-
-#     # Plot the locations with different colors based on the city name
-#     for loc in locations:
-#         city = loc[3]
-#         color = city_colors[city]
-#         folium.Marker(location=validate_location(loc[:2]), icon=folium.Icon(color=color)).add_to(m)
-
-#     # Plot the path between consecutive locations with different colors based on the city name
-#     for i in range(len(locations)-1):
-#         start = validate_location(locations[i][:2])
-#         end = validate_location(locations[i+1][:2])
-#         city = locations[i][3]
-#         color = city_colors[city]
-#         folium.PolyLine([start, end], color=color).add_to(m)
-#     # Create a legend based on the city colors
-#     legend_html = '<div style="position: fixed; bottom: 50px; left: 50px; z-index:9999; font-size:14px;">'
-#     for city, color in city_colors.items():
-#         legend_html += f'<p><span style="background-color:{color};padding:5px;border-radius:50%;">&nbsp;&nbsp;&nbsp;&nbsp;</span> {city}</p>'
-#     legend_html += '</div>'
-#     m.get_root().html.add_child(folium.Element(legend_html))
-#     m.save('playground/templates/map.html')
-
-
 def plot(locations):
     """Plots locations on a map with different colors based on the city name."""
     # Initialize the map
